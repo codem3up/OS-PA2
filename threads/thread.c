c6021535eb1af4819ec14390e6c1d393bc4adad3
pliney <mchambl3@msudenver.edu>
2016-04-12 21:27:56 -0600

    checking on right branch

--- threads/thread.c
+++ threads/thread.c
@@ -14,7 +14,7 @@
 #ifdef USERPROG
 #include "userprog/process.h"
 #endif
-
+/* testing right branch */
 /* Random value for struct thread's `magic' member.
    Used to detect stack overflow.  See the big comment at the top
    of thread.h for details. */
